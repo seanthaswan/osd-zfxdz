0fbdbb2ae06174a20c32aebd48c3998590a4b920
Sean Swanson <sean-swan@hotmail.com>
2020-02-04 13:05:39 -0800

    Removed boilerplate ng-cli template.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    AboutComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule
