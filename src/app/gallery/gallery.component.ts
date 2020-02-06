9f6152471163770c68b1e05e5e33be512447c5e5
Sean Swanson <sean-swan@hotmail.com>
2020-02-05 21:24:13 -0800

    Implemented OpenSeadragon into gallery tile components for DeepZoom imaging. Added tilesource of example image.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Drawing } from "./Drawing.model";
+import * as OpenSeadragon from "openseadragon";
 
 @Component({
   selector: "app-gallery",
@@ -33,7 +34,50 @@ export class GalleryComponent implements OnInit {
       "../../assets/lincoln-sea.jpg"
     )
   ];
-  constructor() {}
+  handleOpenDziButtonClick: any;
+  handleOsdBackButtonClick: any;
+  handleOsdInfoButtonClick: any;
+  osdViewer: HTMLCollection;
+  osdInfoModalContainer: HTMLCollection;
+  viewer: OpenSeadragon;
+  constructor() {
+    this.osdViewer = document.getElementsByClassName("osd-viewer");
+    this.osdInfoModalContainer = document.getElementsByClassName(
+      "controls-info-container"
+    );
+    this.handleOsdInfoButtonClick = $event => {
+      this.osdInfoModalContainer[0].classList.remove("not-displayed");
+    };
+    this.handleOsdBackButtonClick = $event => {
+      // this.viewer.destroy();
+      this.viewer = null;
+      this.osdViewer[0].classList.add("not-displayed");
+    };
+    this.handleOpenDziButtonClick = $event => {
+      this.osdViewer[0].classList.remove("not-displayed");
+      this.viewer = OpenSeadragon({
+        id: "openseadragon",
+        prefixUrl: "node_modules/openseadragon/build/openseadragon/images",
+        tileSources: "../assets/tilesources/lincol-sea-dzi.dzi",
+        showNavigator: false,
+        defaultZoomLevel: 0,
+        minZoomImageRatio: 1,
+        maxZoomPixelRatio: 6,
+        zoomInButton: "osd-zoom-in-button",
+        zoomOutButton: "osd-zoom-out-button",
+        fullPageButton: "osd-full-screen-button",
+        homeButton: "osd-home-button",
+        homeFillsViewer: true,
+        visibilityRatio: 0.8,
+        gestureSettingsMouse: {
+          dblClickToZoom: true,
+          clickToZoom: true
+        },
+        placeholderFillStyle: "rgba(0, 0, 0, 0.2)"
+      });
+      console.log($event);
+    };
+  }
 
   ngOnInit() {}
 }
